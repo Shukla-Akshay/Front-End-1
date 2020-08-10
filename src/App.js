4a79fea6af21f4f52556230f3a936e3994a3eced
Shukla-Akshay <akshay3shukla@gmail.com>
2020-08-10 12:24:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Cards from './Components/Cards';
+import { AppBar, Toolbar } from '@material-ui/core';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+const useStyles = makeStyles({
+  SpaceXContainer: {
+    paddingTop: '20px',
+    paddingRight: '50px',
+    paddingLeft: '50px',
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const classes = useStyles();
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://api.spaceXdata.com/v3/launches?limit=100`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' className={classes.title}>
+            SpaceX
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Cards isLoading={isLoading} items={items} />
     </div>
   );
-}
+};
 
 export default App;
